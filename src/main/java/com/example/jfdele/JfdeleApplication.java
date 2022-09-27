b613e1ab52ad8f8300a3af147cac1b558537ba21
jfmauro <jeff.mauro@gmail.com>
2022-09-27 20:51:15 +0200

    first commit

---
+++ src/main/java/com/example/jfdele/JfdeleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jfdele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfdeleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JfdeleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jfdele/JfdeleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jfdele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfdeleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
